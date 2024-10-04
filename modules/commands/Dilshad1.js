b6e5ac917468a3ca355e9513cc9235354037927b
kh3007749 <kh3007749@gmail.com>
2024-10-04 09:02:41 +0500

    Add files via upload

---
+++ modules/commands/Dilshad1.js
@@ -0,0 +1,29 @@
+ const fs = require("fs");
+module.exports.config = {
+	name: "Owner",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "Owner", 
+	description: "no prefix",
+	commandCategory: "No command marks needed",
+	usages: "...",
+    cooldowns: 100, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("owner") ||
+     react.includes("fb") || 
+react.includes("Owner")) {
+		var msg = {
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𝐏𝐫𝐢𝐧𝐜𝐞 𝐃𝐢𝐥𝐬𝐡𝐚𝐝✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://youtube.com/@MirryKa ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \nTiger 🐅https://www.facebook.com/princedilshad01587 /?ref=share_group_link`",
+				attachment: fs.createReadStream(__dirname + `/noprefix/pro.jpg`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("📷", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+    }
